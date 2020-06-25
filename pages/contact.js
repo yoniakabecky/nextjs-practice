6f8b3454f40b9bb7ba51f145da32afaf2c5b21f7
Yoni <yoniakabecky@gmail.com>
2020-06-25 09:58:22 -0700

    Add hubspot-form

--- pages/contact.js
+++ pages/contact.js
@@ -1,28 +1,36 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import SubPageHero2 from "../components/subPageGsap";
+import HubspotForm from "react-hubspot-form";
 
 const Contact = () => {
-  React.useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "//js.hsforms.net/forms/shell.js";
-    document.body.appendChild(script);
+  // React.useEffect(() => {
+  //   const script = document.createElement("script");
+  //   script.src = "//js.hsforms.net/forms/shell.js";
+  //   document.body.appendChild(script);
 
-    const createForms = () => {
-      if (window.hbspt) {
-        window.hbspt.forms.create({
-          portalId: "7788527",
-          formId: "69a66f1a-5ace-49ab-ae7b-1e768d0bb1b6",
-        });
-      }
-    };
+  //   const createForms = () => {
+  //     if (window.hbspt) {
+  //       window.hbspt.forms.create({
+  //         portalId: "7788527",
+  //         formId: "69a66f1a-5ace-49ab-ae7b-1e768d0bb1b6",
+  //       });
+  //     }
+  //   };
 
-    script.addEventListener("load", createForms);
-  }, []);
+  //   script.addEventListener("load", createForms);
+  // }, []);
+
+  // https://api.hsforms.com/submissions/v3/integration/submit/:portalId/:formGuid
 
   return (
     <Layout>
       <SubPageHero2 title="Contact" subTitle="wanna talk?" />
+      <HubspotForm
+        portalId="7788527"
+        formId="69a66f1a-5ace-49ab-ae7b-1e768d0bb1b6"
+        loading={<div>Loading...</div>}
+      />
     </Layout>
   );
 };
